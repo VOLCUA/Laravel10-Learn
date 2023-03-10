203cd89a900aaa63bbcfde6b4154d04d33988955
VOLCUA <UkraineFastDUB@GMAIL.com>
2023-03-10 20:20:15 +0200

    Add Debugbar to project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = "Code GOOD" ;
+
+    return view('welcome',
+    [
+        'name' => $name
+    ]
+
+);
 });
